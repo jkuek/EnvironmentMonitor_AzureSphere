05a095349f5b6f99c76f4448913a6cacba9560d3
Jeremy K <jeremykuek@live.com>
2019-09-25 22:55:42 +1200

    initial commit for high-level app
    The AvnetAzureSphereSK_OLED app from https://github.com/CloudConnectKits/Azure_Sphere_SK_ADC_RTApp has been used as a launchpad

---
+++ EnvironmentMonitor_HighLevelApp/connection_strings.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Define your connection string here.  The connection string is required to connect to Azure.
+// #define MY_CONNECTION_STRING "HostName=iotc-xxxx....azure-devices.net;DeviceId=xxxx....;SharedAccessKey=xxxxx..."
+
+#define MY_CONNECTION_STRING "HostName=iotc-1a669eb0-bcd0-49a7-85bf-c003c26e5f7d.azure-devices.net;DeviceId=c3c02cd7-855c-4ef8-825e-588dbe8736ef;SharedAccessKey=iy3W2BfhtWJsPmtFR6dNWhSeLpvbdmLVgnID9LhnTdw="
